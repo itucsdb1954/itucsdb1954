649422c3110560b6713068cb208967642ef8f2d7
ozdemirahm <32142519+ozdemirahm@users.noreply.github.com>
2019-12-11 21:03:47 +0300

    database functions added

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import psycopg2 as dbapi2
 from course import Course
 from VF_Cond import Cond
 from Midterm import Midterm
@@ -6,103 +7,238 @@ from Project import Project
 from Attendance import Attendance
 
 class Database:
-    def __init__(self):
-        self.courses = {}
-        self._last_course_key = 0
-        self._last_VF_key=0
-        self._last_Project_key=0
-        self._last_Midterm_key=0
-        self._last_Homework_key=0
-        self._last_Attendance_key=0
-        self.Midterm={ }
-        self.Homework={ }
-        self.Project={ }
-        self.Attendance={ }
+    def __init__(self,dbfile):
+	self.dbfile=dbfile
 
+#course
     def add_course(self,course):
-        self._last_course_key += 1
-        self.courses[self._last_course_key]= course
-        return self._last_course_key
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO courses (department, name, lecturerName, description, VF_conditions) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(query, (course.department, course.name, course.lecturerName, course.description, course.VF_conditions))
+            connection.commit()
+            course_key = cursor.lastrowid
+        return course_key
 
     def delete_course(self,course_key):
-        if course_key in self.courses:
-            del self.courses[course_key]
-            self._last_course_key=self._last_course_key-1
+         with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM courses WHERE (ID = %s)"
+            cursor.execute(query, (course_key,))
+            connection.commit()
 
 
     def get_course(self,course_key):
-        course = self.courses.get(course_key)
-        if course is None:
-            return None
-        course_ = Course(course.name,course.department,course.description,course.lecturerName,course.VF_conditions)
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT department, name, lecturerName, description, VF_conditions FROM courses WHERE (id = %s)"
+            cursor.execute(query, (course_key,))
+            department, name, lecturerName, description, VF_conditions = cursor.fetchone()
+        course_=Course(name,department,description,lecturerName,VF_conditions)
         return course_
 
     def get_courses(self):
         courses = []
-        for course_key,course in self.courses.items():
-            course_=Course(course.name,course.department,course.description,course.lecturerName,course.VF_conditions)
-            courses.append((course_key,course_))
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT id,department, name, lecturerName, description, VF_conditions FROM course ORDER BY ID"
+            cursor.execute(query)
+            for course_key, department,name, lecturerName, description, VF_conditions in cursor:
+            	courses.append((course_key, Course(name, department,description,lecturerName,VF_conditions)))
         return courses
 
-    def update_course(self,course_key,course):
-        self.courses[course_key]=course
-        return
+    def update_course(self,course_key,Course):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE course SET department = %s, name = %s, lecturerName = %s, description = %s, VF_conditions = %s WHERE (ID = %s)"
+            cursor.execute(query, (Course.department,Course.name,Course.lecturerName,Course.description,Course.VF_conditions, course_key))
+            connection.commit()
+        return course_key
 
 
+#Midterm
+	def add_midterm(self,Midterm):
+		with dbapi2.connect(self.dbfile) as connection:
+			cursor = connection.cursor()
+			query = "INSERT INTO midterms(number_of_midterm, midterm_weight, midterm_score1,midterm_score2,is_important) VALUES (%s, %s, %s, %s, %s)"
+			cursor.execute(query, (Midterm.number_of_midterm, Midterm.midterm_weight, Midterm.midterm_score[0],Midterm.midterm_score[1], Midterm.is_important))
+			connection.commit()
+			midterm_key = cursor.lastrowid
+		return midterm_key
 
+    def delete_course(self,midterm_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM midterms WHERE (ID = %s)"
+            cursor.execute(query, (midterm_key,))
+            connection.commit()
 
-    def add_Midterm(self,Midterm):
-        self._last_Midterm_key+=1
-        self.Midterm[self._last_Midterm_key]=Midterm
-        return self._last_Midterm_key
-
-    def get_midterm(self,course_key):
-        midterm = self.Midterm.get(course_key)
-        if midterm is None:
-            return None
-        midterm_ = Midterm(midterm.number_of_midterm,midterm.midterm_weight,midterm.is_important,course_key)
+    def get_midterm(self,midterm_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT number_of_midterm, midterm_weight, midterm_score1,midterm_score2,is_important FROM midterms WHERE (id = %s)"
+            cursor.execute(query, (midterm_key,))
+            number_of_midterm, midterm_weight, midterm_score1,midterm_score2,is_important = cursor.fetchone()
+            midterm_ = Midterm(number_of_midterm, midterm_weight,is_important)
+            midterm_.midterm_score[0]=midterm_score1
+            midterm_.midterm_score[1]=midterm_score2
         return midterm_
-    def add_midterm_score(self,midterm_score,course_key):
-        self.Midterm[course_key].midterm_score[0]=midterm_score[0]
-        self.Midterm[course_key].midterm_score[1]=midterm_score[1]
-        return
+    """def get_midterms(self):
+        midterms = []
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT id,number_of_midterm, midterm_weight, midterm_score1,midterm_score2,is_important FROM midterms ORDER BY id"
+            cursor.execute(query)
+            for midterm_key, number_of_midterm, midterm_weight, midterm_score1,midterm_score2,is_important in cursor:
+                midterms.append((midterm_key, Midterm(number_of_midterm, midterm_weight,is_important)))
+                midterms[midterm_key].midterm_score[0]=midterm_score1
+        		midterms[midterm_key].midterm_score[1]=midterm_score2
+        return midterms
+	"""
+    def update_midterm(self,midterm_key,Midterm):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE midterms SET number_of_midterm = %s, midterm_weight = %s, midterm_score1 = %s, midterm_score2 = %s, is_important = %s WHERE (id = %s)"
+            cursor.execute(query, (Midterm.number_of_midterm,Midterm.midterm_weight,Midterm.midterm_score[0],Midterm.midterm_score[1],Midterm.is_important, midterm_key))
+            connection.commit()
+        return midterm_key
 
+#Homeworks
+    def add_homework(self,Homework):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO homeworks(number_of_homework, homework_weight, homework_score1,homework_score2,homework_score3,homework_score4,is_important) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(query, (Homework.number_of_homework, Homework.homework_weight, Homework.homework_score[0], Homework.homework_score[1], Homework.homework_score[2], Homework.homework_score[3], Homework.is_important))
+            connection.commit()
+            homework_key = cursor.lastrowid
+        return homework_key
 
-    def add_Homework(self,Homework):
-        self._last_Homework_key+=1
-        self.Homework[self._last_Homework_key]=Homework
-        return self._last_Homework_key
-    def get_homework(self,course_key):
-        homework = self.Homework.get(course_key)
-        if homework is None:
-            return None
-        homework_ = Homework(homework.number_of_homework,homework.homework_weight,homework.is_important,course_key)
+    def delete_homework(self,homework_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM homeworks WHERE (ID = %s)"
+            cursor.execute(query, (homework_key,))
+            connection.commit()
+
+    def get_homework(self,homework_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT number_of_homework, homework_weight, homework_score1,homework_score2,homework_score3,homework_score4,is_important FROM homeworks WHERE (id = %s)"
+            cursor.execute(query, (homework_key,))
+            number_of_homework, homework_weight, homework_score1,homework_score2,homework_score3,homework_score4,is_important = cursor.fetchone()
+            homework_ = Homework(number_of_homework, homework_weight,is_important)
+            homework_.homework_score[0]= homework_score1
+            homework_.homework_score[1]= homework_score2
+            homework_.homework_score[2]= homework_score3
+            homework_.homework_score[3]= homework_score4
         return homework_
+        """ def get_homeworks(self):
+        homeworks = []
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT id,number_of_homework, homework_weight, homework_score1,homework_score2,homework_score3,homework_score4,is_important FROM homeworks ORDER BY ID"
+            cursor.execute(query)
+            for homework_key,number_of_homework, homework_weight, homework_score1,homework_score2,homework_score3,homework_score4,is_important in cursor:
+                homeworks.append((homework_key, Homework(number_of_homework, homework_weight,is_important)))
+                homeworks[homework_key].homework_score[0]= homework_score1
+        		homeworks[homework_key].homework_score[1]= homework_score2
+        		homeworks[homework_key].homework_score[2]= homework_score3
+        		homeworks[homework_key].homework_score[3]= homework_score4
+        return homeworks
+"""
+	def update_homework(self,homework_key,Homework):
+		with dbapi2.connect(self.dbfile) as connection:
+			cursor = connection.cursor()
+			query = "UPDATE homeworks SET number_of_homework = %s, homework_weight = %s, homework_score1 = %s, homework_score2 = %s, homework_score3 = %s, homework_score4 = %s, is_important = %s WHERE (ID = %s)"
+			cursor.execute(query, (Homework.number_of_homework,Homework.homework_weight,Homework.homework_score[0],Homework.homework_score[1],Homework.homework_score[2],Homework.homework_score[3],Homework.is_important, midterm_key))
+			connection.commit()
+		return homework_key
+#project
+
+    def add_project(self,Project):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO projects(number_of_project,project_weight,project_score1,project_score2,is_important) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(query, (Project.number_of_project, Project.project_weight, Project.project_score[0], Project.project_score[1], Project.is_important))
+            connection.commit()
+            project_key = cursor.lastrowid
+        return project_key
+
+    def delete_project(self,project_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM projects WHERE (ID = %s)"
+            cursor.execute(query, (project_key,))
+            connection.commit()
+
+    def get_project(self,project_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT number_of_project,project_weight,project_score1,project_score2,is_important FROM projects WHERE (id = %s)"
+            cursor.execute(query, (project_key,))
+            number_of_project,project_weight,project_score1,project_score2,is_important = cursor.fetchone()
+            project_ = Project(number_of_project,project_weight,project_score1,project_score2,is_important)
+    	return project_
+
+    """def get_projects(self):
+        projects = []
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT id,number_of_project,project_weight,project_score1,project_score2,is_important FROM projects ORDER BY ID"
+            cursor.execute(query)
+            for project_key,number_of_homework, homework_weight, homework_score1,homework_score2,homework_score3,homework_score4,is_important in cursor:
+                projects.append((project_key, Project(number_of_project,project_weight,project_score1,project_score2,is_important)))
+        return projects
+	"""
+    def update_project(self,project_key,Project):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE projects SET number_of_project = %s, project_weight = %s, project_score1 = %s, project_score2 = %s,is_important = %s WHERE (ID = %s)"
+            cursor.execute(query, (Project.number_of_project,Project.project_weight,Project.project_score[0],Project.project_score[1],Project.is_important, project_key))
+            connection.commit()
+        return project_key
 
 
+#attendance
+    def add_attendance(self,Attendance):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO attendances(upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s)"
+            cursor.execute(query, (Attendance.upper_limit_percent, Attendance.attendance[0], Attendance.attendance[1], Attendance.project_score[1], Attendance.project_score[2], Attendance.project_score[3], Attendance.project_score[4], Attendance.project_score[5], Attendance.project_score[6], Attendance.project_score[7], Attendance.project_score[8], Attendance.project_score[9], Attendance.project_score[10], Attendance.project_score[11], Attendance.project_score[12], Attendance.project_score[13],Attendance.is_important))
+            connection.commit()
+            attendance_key = cursor.lastrowid
+        return attendance_key
 
-    def add_Project(self,Project):
-        self._last_Project_key+=1
-        self.Project[self._last_Project_key]=Project
-        return self._last_Project_key
+    def delete_attendance(self,attendance_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM attendances WHERE (ID = %s)"
+            cursor.execute(query, (attendance_key,))
+            connection.commit()
 
-    def get_project(self,course_key):
-        project = self.Project.get(course_key)
-        if project is None:
-            return None
-        project_ = Project(project.number_of_project,project.project_weight,project.is_important,course_key)
-        return project_
-
-
-
-    def add_Attendance(self,Attendance):
-        self._last_Attendance_key+=1
-        self.Attendance[self._last_Attendance_key]=Attendance
-        return self._last_Attendance_key
-
-    def get_attendance(self,course_key):
-        attendance = self.Attendance.get(course_key)
-        if attendance is None:
-            return None
-        attendance_ = Attendance(attendance.upper_limit_percent,attendance.is_important,course_key)
+    def get_attendance(self,attendance_key):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important FROM attendances WHERE (id = %s)"
+            cursor.execute(query, (attendance_key,))
+            upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important = cursor.fetchone()
+        attendance_ = Attendance(upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important)
         return attendance_
+
+    """def get_attendances(self):
+        attendances = []
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT id,upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important FROM attendances ORDER BY ID"
+            cursor.execute(query)
+            for attendances_key,upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important in cursor:
+                attendances.append((attendances_key, Project(upper_limit_percent,attendance_hour1,attendance_hour2,attendance_hour3,attendance_hour4,attendance_hour5,attendance_hour6,attendance_hour7,attendance_hour8,attendance_hour9,attendance_hour10,attendance_hour11,attendance_hour12,attendance_hour13,attendance_hour14,is_important)))
+        return attendances
+	"""
+    def update_attendances(self,attendance_key,Attendance):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE attendances SET upper_limit_percent = %s, attendance_hour1 = %s, attendance_hour2 = %s,attendance_hour3 = %s,attendance_hour4 = %s,attendance_hour5 = %s,attendance_hour6 = %s,attendance_hour7 = %s,attendance_hour8 = %s,attendance_hour9 = %s,attendance_hour10 = %s,attendance_hour11 = %s,attendance_hour12 = %s,attendance_hour13 = %s,attendance_hour14 = %s,is_important = %s WHERE (ID = %s)"
+            cursor.execute(query, (Attendance.upper_limit_percent,Attendance.attendance[0],Attendance.attendance[1],Attendance.attendance[2],Attendance.attendance[3],Attendance.attendance[4],Attendance.attendance[5],Attendance.attendance[6],Attendance.attendance[7],Attendance.attendance[8],Attendance.attendance[9],Attendance.attendance[10],Attendance.attendance[11],Attendance.attendance[12],Attendance.attendance[13],Attendance.is_important, attendance_key))
+            connection.commit()
+        return attendance_key
