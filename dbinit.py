0fdc287e93a408ca0f0d4e103a775fdb61442d7e
ozdemirahm <32142519+ozdemirahm@users.noreply.github.com>
2019-12-15 12:35:36 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -122,22 +122,6 @@ ALTER TABLE vf_conditions ADD CONSTRAINT num5 FOREIGN KEY(id) REFERENCES courses
 """
 ,
 """
-ALTER TABLE vf_conditions ADD CONSTRAINT num6 FOREIGN KEY(attendance) REFERENCES attendances(id);
-"""
-,
-"""
-ALTER TABLE vf_conditions ADD CONSTRAINT num7 FOREIGN KEY(midterm) REFERENCES midterms(id);
-"""
-,
-"""
-ALTER TABLE vf_conditions ADD CONSTRAINT num8 FOREIGN KEY(homework) REFERENCES homeworks(id);
-"""
-,
-"""
-ALTER TABLE vf_conditions ADD CONSTRAINT num9 FOREIGN KEY(project) REFERENCES projects(id);
-"""
-,
-"""
 ALTER TABLE user_course ADD CONSTRAINT num10 FOREIGN KEY(user_course_id) REFERENCES  users(id);
 """
 ,
