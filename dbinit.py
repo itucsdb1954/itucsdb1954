14028b8132777653e6b962448749775a03a758ce
ozdemirahm <32142519+ozdemirahm@users.noreply.github.com>
2019-12-15 17:09:57 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -107,6 +107,10 @@ ALTER TABLE homeworks ADD CONSTRAINT num2 FOREIGN KEY(id) REFERENCES courses(id)
 ALTER TABLE midterms ADD CONSTRAINT num3 FOREIGN KEY(id) REFERENCES courses(id);
 ALTER TABLE projects ADD CONSTRAINT num4 FOREIGN KEY(id) REFERENCES courses(id);
 ALTER TABLE vf_conditions ADD CONSTRAINT num5 FOREIGN KEY(id) REFERENCES courses(id);
+ALTER TABLE vf_conditions ADD CONSTRAINT num6 FOREIGN KEY(attendance) REFERENCES attendances(id);
+ALTER TABLE vf_conditions ADD CONSTRAINT num7 FOREIGN KEY(midterm) REFERENCES midterms(id);
+ALTER TABLE vf_conditions ADD CONSTRAINT num8 FOREIGN KEY(homework) REFERENCES homeworks(id);
+ALTER TABLE vf_conditions ADD CONSTRAINT num9 FOREIGN KEY(project) REFERENCES projects(id);
 ALTER TABLE user_course ADD CONSTRAINT num10 FOREIGN KEY(user_course_id) REFERENCES  users(id);
 ALTER TABLE user_course ADD CONSTRAINT num11 FOREIGN KEY(course_no) REFERENCES courses(id);
 """
