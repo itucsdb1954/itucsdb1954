503ccee6c3f5561096b39855884b3b73a5f4d5a0
Alpermeric <32162983+Alpermeric@users.noreply.github.com>
2019-12-15 11:51:06 +0300

    Update VF_Cond.py

--- VF_Cond.py
+++ VF_Cond.py
@@ -1,7 +1,6 @@
 class Cond:
-    def __init__(self,attendance,midterm,homework,project,course_key):
+    def __init__(self,attendance,midterm,homework,project):
         self.attendance=attendance
         self.midterm=midterm
         self.homework=homework
         self.project=project
-        self.id=course_key
